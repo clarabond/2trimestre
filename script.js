872838180a3cc94c9f2fd2d1b23041d7644dbabb
clarabond <128716837+clarabond@users.noreply.github.com>
2024-05-28 08:48:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,78 @@
-const'  1'                        
+cost caixaPrincipal=document.querySelector(".caixa-principal")
+cost caixaPerguntas=document.querySelector(".caixa-perguntas")
+cost caixaAlternativas=document.querySelector(".caixa-alternativas")
+cost caixaResultado=document.querySelector(".caixa-resultado")
+cost textoResultado=document.querySelector(".texto-resultado")
+
+cost perguntas=[
+  {
+     enunciado:"Pergunta 1",
+    alternativas:[
+       "Alternativa 1"
+       "Alternativa 2"
+  ]
+  },     
+  {
+     enunciado:"Pergunta 2",
+    alternativas:[
+       "Alternativa 1"
+       "Alternativa 2"
+  ]
+  }, 
+  {
+     enunciado:"Pergunta 3",
+    alternativas:[
+       "Alternativa 1"
+       "Alternativa 2"
+  ]
+  },  
+  {
+     enunciado:"Pergunta 4",
+    alternativas:[
+       "Alternativa 1"
+       "Alternativa 2"
+  ]
+  },           
+  {
+     enunciado:"Pergunta 5",
+    alternativas:[
+       "Alternativa 1"
+       "Alternativa 2"
+  ]
+  },           
+  ];
+ {
+     enunciado:"Você gosta de estudar?",
+    alternativas:[
+       "Não"
+       "Sim"
+  ]
+  },     
+  {
+     enunciado:"Você é mais de exatas ou humanas?",
+    alternativas:[
+       "Exatas"
+       "Humanas"
+  ]
+  }, 
+  {
+     enunciado:"O que você prefere?",
+    alternativas:[
+       "Doces"
+       "Salgadas"
+  ]
+  },  
+  {
+     enunciado:"",
+    alternativas:[
+       "Alternativa 1"
+       "Alternativa 2"
+  ]
+  },           
+  {
+     enunciado:"Pergunta 5",
+    alternativas:[
+       "Alternativa 1"
+       "Alternativa 2"
+  ]
+  },           
